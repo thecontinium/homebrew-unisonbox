ea27550c4ae2430086f6abb18276448673a2db7d
The Continium <thecontinium@outlook.com>
2014-11-06 07:50:13 +1100

    initial plist

---
+++ Formula/unisonbox-client.rb
@@ -0,0 +1,45 @@
+class BrewUpdate < Formula
+  homepage 'https://github.com/thecontinium/homebrew-unisonbox/'
+  url 'https://github.com/thecontinium/homebrew-unisonbox.git'
+  version '0.0.1'
+
+  skip_clean 'bin'
+
+  depends_on "unisonbox"
+  
+  def install
+  end
+
+  #plist_options :startup => true
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Disabled</key>
+      <false/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/bin/ssh</string>
+        <string>server-user@server-ip</string>
+        <string>touch</string>
+        <string>UnisonBox/.clientchange</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/tmp/#{plist_name}.err</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/#{plist_name}.out</string>
+      <key>WatchPaths</key>
+      <array>
+        <string>/Users/client-user/UnisonBox</string>
+      </array>
+    </dict>
+    </plist>
+    EOS
+  end
+end
