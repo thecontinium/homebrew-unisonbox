9ac3201c38d58b7e9059190d9533e42cb03ffb0d
The Continium <thecontinium@outlook.com>
2014-11-06 10:00:35 +1100

    initial server files

---
+++ Formula/unisonbox-server.rb
@@ -0,0 +1,56 @@
+class UnisonboxServer< Formula
+  homepage 'https://github.com/thecontinium/homebrew-unisonbox/'
+  url 'https://github.com/thecontinium/homebrew-unisonbox.git'
+  version '0.0.1'
+
+  skip_clean 'bin'
+
+  depends_on "unison"
+  depends_on "terminal-notifier"
+  
+  def install
+    bin.install 'unisonbox-server'
+  end
+
+  def caveats; <<-EOS.undent
+    Edit the plist file with correct settings. 
+    EOS
+  end
+
+
+  #plist_options :startup => true
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>AbandonProcessGroup</key>
+      <true/>
+      <key>Disabled</key>
+      <false/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/local/bin/unisonbox-server</string>
+        <string>#{prefix}unisonbox.prf</string>
+        <string>server-user</string>
+        <string>server-ip</string>
+        <string>notification-email</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/tmp/#{plist_name}.err</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/#{plist_name}.out</string>
+      <key>WatchPaths</key>
+      <array>
+        <string>/Users/server-user/UnisonBox</string>
+      </array>
+    </dict>
+    </plist>
+    EOS
+  end
+end
