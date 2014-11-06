689d49c9413a523c328fe1d8a006d779bd1c2602
The Continium <thecontinium@outlook.com>
2014-11-06 12:48:47 +1100

    .

--- Formula/unisonbox-server.rb
+++ Formula/unisonbox-server.rb
@@ -1,7 +1,7 @@
 class UnisonboxServer< Formula
   homepage 'https://github.com/thecontinium/homebrew-unisonbox/'
   url 'https://github.com/thecontinium/homebrew-unisonbox.git'
-  version '0.0.1'
+  version '0.0.2'
 
   skip_clean 'bin'
 
